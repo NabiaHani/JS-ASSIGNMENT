2b120cb6225862b246e22985278d4783e5060d78
NabiaHani <127650823+NabiaHani@users.noreply.github.com>
2023-10-04 12:31:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// Practice exercise 1.3
+var a = 10;
+let b = 20;
+const c = "nabia";
+
+var d = 25;
+let e = 20;
+var f = d + e;
+console.log(f);
+
+
+alert("HI NABIA!");
+
+
+prompt("what is your name?");
+
+
+let n = prompt("what is your name");
+console.log(n);
+
+
+console.log(Math.random());
+
+console.log(Math.random()*2000);
